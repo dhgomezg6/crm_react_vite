7ed1856406efde5f7f05cc5f948c003b82d54663
dinl <dinl_@hotmail.com>
2023-03-13 23:11:32 -0500

    Up to show client list

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index, { loader as clientesLoader} from './pages/Index'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      },
+      {
+        path: '/hola',
+        element: <h2>Hola</h2>
+      }
+    ]
+  }
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
